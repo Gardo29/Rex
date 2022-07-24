bde8edf1dcfec1677ad6e6627d27fd247e9ae67b
Lorenzo Gardini <root@DESKTOP-U06GU4A.localdomain>
2022-07-24 08:44:56 +0200

    added surprise to requires in setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     author_email='l.gardo98@gmail.com',
     description='',
     install_requires=['numpy',
+                      'surprise'
                       'pandas',
                       'requests',
                       'fastapi',
