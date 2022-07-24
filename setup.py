8d0c9ce0a9bdb6e350f9b8b8df3d7b93adce26fd
Lorenzo Gardini <root@DESKTOP-U06GU4A.localdomain>
2022-07-24 08:42:48 +0200

    package_data

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=['rex', 'test', 'test.check_tests', 'test.model_tests', 'test.preprocessing_tests',
               'test.model_selection_tests'],
     package_dir={'': 'src'},
+    package_data={'src': ['/*']},
     url='',
     license='',
     author='Lorenzo',
